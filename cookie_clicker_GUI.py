8db215f6de12ea14c6b6368f65d368e67cc5819a
sc1341 <33675337+sc1341@users.noreply.github.com>
2019-01-08 21:37:59 -0600

    Update cookie_clicker_GUI.py

--- cookie_clicker_GUI.py
+++ cookie_clicker_GUI.py
@@ -1,9 +1,19 @@
 from tkinter import *
 from image import image as cookieimage
+import time
+
+
+class UpgradesController:
+
+    def __init__(self, data):
+        #LOGIC TO PARSE DATA GOES HERE
+        self.auto_click_upgrade = 0
+
 
 class CookieClickerMainGUI:
 
     def __init__(self, root, data=''):
+        self.upgrade_controller = UpgradesController(data)
         root.geometry("700x700")
         root.title("Cookie Clicker")
         self.photo = PhotoImage(data=cookieimage)
@@ -13,7 +23,7 @@ class CookieClickerMainGUI:
         self.score = self.check_data(self.data)
         self.label = Label(root,text="Hello",font=("Helvetica", 16))
         self.cookie_button = Button(root,text="click me!", command=self.cookie_clicked, image=self.photo)
-        self.upgrades_button = Button(root,text="Upgrades",command=self.upgrades_menu)
+        self.upgrades_button = Button(root,text="Upgrades",command=self.upgrades_GUI_menu)
         self.score_label = Label(root,text=str(self.score),font=("Helvetica", 20))
 
         #pack the widgets inside the screen
@@ -37,13 +47,25 @@ class CookieClickerMainGUI:
         self.score_label.config(text=str(self.score))
         print("Button clicked: the score is: " + str(self.score))
 
-    def upgrades_menu(self):
+    def upgrades_GUI_menu(self):
         '''This creates a pop-up window to modify your upgrades, it is easy to add things from here'''
         self.root2 = Tk()
         self.root2.geometry("500x300")
         self.root2.title("Upgrades Menu")
-        self.auto_click = Button(self.root2, text="Buy Auto-click")
-        self.auto_click.pack()
+        self.auto_click_button = Button(self.root2, text="Buy Auto-click", command=self.auto_click)
+        self.auto_click_button.pack()
         self.root2.mainloop()
+
+    def auto_click(self):
+        '''Sets the autoclick to run, it subtracts the score by 100 times the number of upgrades that the auto_click 
+        has, it then sets the updatecontroller.aut_click_upgrade += 1'''
+        self.score -= 100 * self.upgrade_controller.auto_click_upgrade
+        self.upgrade_controller.auto_click_upgrade += 1
+        while True:
+            #THis is a known issue that causes the program to freeze than crash, concider time.time?
+            sleep(60-self.upgrade_controller.auto_click_upgrade)
+            self.score += 1
+
+
 
 
