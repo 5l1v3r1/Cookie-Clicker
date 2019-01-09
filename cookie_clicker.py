9a747baa95458f02c2fb8e0f35bedf409a95a8f3
sc1341 <33675337+sc1341@users.noreply.github.com>
2019-01-08 19:39:37 -0600

    Add files via upload
    Works... 1/8/19

---
+++ cookie_clicker.py
@@ -0,0 +1,114 @@
+from tkinter import *
+import os #make the folder
+import pickle #save the data 
+from time import sleep #sleep function
+import getpass #gets the username from the computer
+import subprocess
+import sys
+from cookie_clicker_GUI import CookieClickerMainGUI
+
+python_file_name = sys.argv[0]
+
+
+
+#global variables
+global username  #gets the username for changing directories
+username = getpass.getuser()
+
+
+'''
+Order things must be run in:
+
+1. change_directory_cookie_folder()
+2. import_data()
+3. Create object and run __init__
+4.class takes over with all of the funcitons regarding the actual game, export has to be inside class?? check this
+
+INFO: 
+
+The CookieClickerGUI is the class that runs the GUI, it takes two functions, the root tk() class and the data
+, the data comes from the import_data function where it takes all data and changes the score if the data is present, if 
+not, then the score sets to zero. This program MUST be run on the desktop at the moment
+
+NOTES TO SELF:
+
+the pack () function can take an arg but it throws an error, you have to tell the widget itself where it should go
+
+
+'''
+
+
+def import_data():
+    '''Trys to import existing data from the folder, if not, create it with
+    create_dat_file function'''
+    data = ''
+    try:
+        os.chdir("C://Users/{}/Desktop/cookie_clicked".format(username))
+    except FileNotFoundError:
+        try:
+            os.chdir("/users/{}/Desktop/cookie_clicked".format(username))
+        except:
+            print("Fatal error")
+    try:
+        files = subprocess.check_output(['ls'])
+        for file in files:
+            if file == "cookie.dat":
+                print("Data dump found")
+                with open('cookie.dat') as f:
+                    data = pickle.load(f)
+                    f.close()
+            else:
+                create_dat_file()
+    except:
+        pass
+    return data
+
+
+def create_dat_file():
+    '''Creates the dat file for exporting and importing'''
+    f = open('cookie.dat','wb')
+    f.close()
+    print("file created")
+    
+
+def export_data(data):
+    '''Make this happen every minute or something'''
+    pass
+    
+
+    
+def change_directory_cookie_folder():
+    '''works on mac and windows, changes the directory to the cookie folder, if dir does not exist
+    it creates it, it supports the file structures of mac and windows'''
+    try:
+        os.chdir("C://Users/{}/Desktop/cookie_clicked".format(username))
+    except FileNotFoundError:
+        try:
+            os.mkdir("C://Users/{}/Desktop/cookie_clicked".format(username))
+        except FileNotFoundError:
+            try:
+                os.mkdir("/users/{}/Desktop/cookie_clicked".format(username))
+            except FileExistsError:
+                os.chdir("/users/{}/Desktop/cookie_clicked".format(username))
+        
+    
+def main():
+    change_directory_cookie_folder()
+    data = import_data()
+    root = Tk()
+    window = CookieClickerMainGUI(root, data)
+    root.mainloop()
+
+
+def test_cases():
+    print(score)
+    print(username)
+    print(str(os.getcwd()))
+
+
+if __name__ == '__main__':
+    main()
+
+
+ 
+        
---
+++ cookie_clicker_GUI.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+
+class CookieClickerMainGUI:
+
+    def __init__(self, root, data=''):
+        root.geometry("700x700")
+        root.title("Cookie Clicker")
+        #Inits the score for the game, will change if .dat file exists
+        self.score = 0
+        self.data = data
+        self.score = self.check_data(self.data)
+        self.label = Label(root,text="Hello",font=("Helvetica", 16))
+        self.cookie_button = Button(root,text="click me!", command=self.cookie_clicked)
+        self.upgrades_button = Button(root,text="Upgrades",command=self.upgrades_menu)
+        self.score_label = Label(root,text=str(self.score),font=("Helvetica", 20))
+
+        #pack the widgets inside the screen
+        self.score_label.pack()
+        self.label.pack()
+        self.cookie_button.pack()
+        self.upgrades_button.pack()
+        
+    def check_data(self,data):
+        '''This checks the data if there was any and updates to the previous score, else it does nothing'''
+        if self.data == '':
+            self.score = 0
+        else:
+            self.score = int(data)
+        return self.score
+
+    def cookie_clicked(self):
+        self.score += 1
+        self.score_label.config(text=str(self.score))
+        print("Button clicked: the score is: " + str(self.score))
+        #self.label.after(500, str(score))
+
+    def upgrades_menu(self):
+        '''This creates a pop-up window to modify your upgrades, it is easy to add things from here'''
+        self.root2 = Tk()
+        self.root2.geometry("500x300")
+        self.root2.title("Upgrades Menu")
+        self.auto_click = Button(self.root2, text="Buy Auto-click")
+        self.auto_click.pack()
+        self.root2.mainloop()
+
+
+
+
+
