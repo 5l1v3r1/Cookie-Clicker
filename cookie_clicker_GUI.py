b39154de689d62f6a143dfa82fce8dfae85c0baa
sc1341 <33675337+sc1341@users.noreply.github.com>
2019-01-12 13:53:13 -0600

    Update cookie_clicker_GUI.py

--- cookie_clicker_GUI.py
+++ cookie_clicker_GUI.py
@@ -17,6 +17,7 @@ class UpgradesController:
             self.auto_click_upgrade = 0
             #All upgrades have to be 1 by default, tis is due to the logic of the game using the upgrade number to determine the cost
             self.cookies_per_click_upgrade = 1
+            self.random_bonus_upgrade = 0
 
 
 class CookieClickerMainGUI:
@@ -46,6 +47,8 @@ class CookieClickerMainGUI:
         self.cookie_button.pack()
         self.upgrades_button.pack()
         self.save_button.pack()
+
+    # ----------------------- EXPORT DATA --------------------------------------------
         
         
     def export_data(self):
@@ -57,32 +60,42 @@ class CookieClickerMainGUI:
         with open('cookie.dat','wb') as f:
             pickle.dump(export_data,f)
         f.close()
-
+    # ----------------------- MAIN COOKIE CLICKED --------------------------------------------
 
     def cookie_clicked(self):
-        '''Adds to the score when the cookie button is clicked, it then updates the label... the defualt it 
-        the label updates ever 200 milliseconds, do not change this!'''
+        '''Adds to the score when the cookie button is clicked, it then updates the label and prints out the results'''
         self.score += self.upgrade_controller.cookies_per_click_upgrade
         self.score_label.config(text=str(self.score))
         print("Button clicked: the score is: " + str(self.score))
 
 
 
-    # ----------------------- MENU WINDOWS --------------------------------------------
+    # ----------------------- MENU WINDOW --------------------------------------------
 
     def upgrades_GUI_menu(self):
         '''This creates a pop-up window to modify your upgrades, it is easy to add more buttons/upgrades from here'''
+
+        #Creates our root window
         self.root2 = Tk()
         self.root2.geometry("500x300")
         self.root2.title("Upgrades Menu")
-        self.auto_click_button = Button(self.root2, text="Buy Auto-click: $" + str(100 * self.upgrade_controller.auto_click_upgrade), command=self.auto_click)
-        self.cookies_per_click_button = Button(self.root2, text="Buy more cookies per click: $" + str(20 * self.upgrade_controller.cookies_per_click_upgrade,
-        command=self.cookies_per_click))
+
+        #Prices of the upgrades, this saves space in the "text" part of the button creation
+        auto_click_price = "Buy Auto-click: $" + str(100 * self.upgrade_controller.auto_click_upgrade)
+        cookies_per_click_price = "Buy more cookies per click: $" + str(20 * self.upgrade_controller.cookies_per_click_upgrade)
+        random_bonus_price = "Buy Random Bonus: $" + str(10000)
+
+        #Buttons
+
+        self.auto_click_button = Button(self.root2, text=auto_click_price, command=self.auto_click)
+
+        self.cookies_per_click_button = Button(self.root2, text=cookies_per_click_price,command=self.cookies_per_click)
         
         self.random_bonus_button = Button(self.root2, text="Random Bonus",command=self.random_bonus)
         #Pack the buttons
         self.cookies_per_click_button.pack()
         self.auto_click_button.pack()
+        self.random_bonus_button.pack()
         self.root2.mainloop()
 
     # ----------------------- UPGRADE FUNCTIONS --------------------------------------------
@@ -96,24 +109,26 @@ class CookieClickerMainGUI:
     def auto_click(self):
         '''Sets the autoclick to run, it subtracts the score by 100 times the number of upgrades that the auto_click 
         has, it then sets the updatecontroller.auto_click_upgrade += 1'''
+
         self.score -= 100 * self.upgrade_controller.auto_click_upgrade
         self.upgrade_controller.auto_click_upgrade += 1
         self.score_label.config(text=str(self.score))
         t1 = threading.Thread(target=self.auto_click_thread_run)
-        #Possibly open a thread here?
-        '''
-        while True:
-            #THis is a known issue that causes the program to freeze than crash, concider time.time?
-            time1 = time.time()
-            if (sum(time1) + 1) == time.time():    
-                self.score += 1 '''
+        t1.start()
+        self.auto_click_button.pack_forget()
+
+        
+
     def random_bonus(self):
-        '''Random bonus of 1000 cookies, it can only be purchased once'''
+        '''Random bonus of 1000 cookies, it can only be purchased once, it costs 10,000 cookies, this function is called when the button is pressed,
+        however it does not actaully do the logic of the random cookie, but rather starts a thread so we can use time.sleep without python crashing'''
+        self.score -= 10000
+        self.upgrade_controller.random_bonus_upgrade += 1
+        self.score_label.config(text=str(self.score))
+        random_thread = threading.Thread(target=self.random_bonus_thread_run)
+        random_thread.start()
         self.random_bonus_button.pack_forget()
-        while True:
-            if 10 == random.randint(1,100):
-                self.score += 1000
-            
+
             
     def no_negative(self):
         while True:
@@ -122,29 +137,19 @@ class CookieClickerMainGUI:
                 self.cookie_debt_label.pack()
             else:
                 pass
+
+    # ----------------------- THREADED FUNCTIONS --------------------------------------------
+
     def auto_click_thread_run(self):
-        pass
-                
-    
-'''
-Added mac and windows feature so the file check works
-Updated docs
-Removed import_data form the GUI class, it is messy and dumb to do it that way, the data must be parsed before even entering the object
-Removed calloing the export function from the cookie_clicked function because it slows down the program noticlbly to call revusive function in tkinter
-Do not need line 10 in cookie_clicker
-Do not need sys module at the moment in cookie_clicker or the GUI
+    	while True:
+    		time.sleep(1)
+    		self.score += 1
+    		self.score_label.config(text=str(self.score))
 
-Problems:
-    the dat file is not creating WTF is happening there?
+    def random_bonus_thread_run(self):
+        while True:
+            time.sleep(1)
+            if 10 == random.randint(1,100):
+                self.score += 1000
+                print("Here are 100 random cookies... yay!")
     
-    
-Can we open a sleeper thread that takes a perameter? 
-
-options:
-    thread that uses how many times it was bought to increase price and abilities
-    
-    sleeper thread to run the function normally
-    
-    
-
-'''
